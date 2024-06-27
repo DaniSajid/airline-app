baec57674ffbd163469b56bc7ccec528149d065d
Daniyal <dani.play49@gmail.com>
2024-06-27 03:05:50 -0700

    Project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { DestinationComponent } from './destination/destination.component';
+import { DestinationDetailComponent } from './destination-detail/destination-detail.component';
+import { LoginSignupComponent } from './login-signup/login-signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', title:'Home' , component:HomeComponent},
+    {path: 'about', title:'About' , component:AboutComponent},
+    {path: 'contact', title:'Contact' , component:ContactComponent},
+    {path: 'destination', title:'Destination' , component:DestinationComponent},
+    {path: 'destinationDetail', title:'DestinationDetail' , component:DestinationDetailComponent},
+    {path: 'login', title:'Account' , component:LoginSignupComponent},
+];
